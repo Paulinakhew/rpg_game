ae853f270c4c47f041d2c85c52345702edb568ad
Esther Khew <paulinakhew12345@hotmail.com>
2018-10-15 15:28:10 -0400

    modularized code into view function

---
+++ view.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def showInstructions():
+    #print a main menu and the commands
+    print('''
+RPG Game
+========
+
+Get to the Garden with a key and a potion. 
+Avoid the monsters!
+
+Commands:
+  go [direction]
+  get [item]
+''')
