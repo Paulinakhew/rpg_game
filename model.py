a9701faaeb098b232212c264e78b66cf0becc0fd
Esther Khew <paulinakhew12345@hotmail.com>
2018-11-02 23:02:20 -0400

    deleted useless function

--- model.py
+++ model.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-def initial_health():
-    health_points = 125
-    return health_points
-
 def fight_monster(health_points):
     health_points -= 25
     return health_points
